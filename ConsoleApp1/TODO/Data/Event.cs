dccc7a82fc15ca2877ebd48cb4719d62a5b4d823
stark <starkov123123@gmail.com>
2019-11-03 00:41:05 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/TODO/Data/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.TODO.Data
+{
+    class Event : IEnumerable, IEnumerator
+    {
+        public object Current => throw new NotImplementedException();
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
